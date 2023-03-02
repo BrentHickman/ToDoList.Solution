ca4637bef929e82e6f29e323f290cf196c3e7123
BrentHickman <BrentHickman@ymail.com>
2023-03-02 09:18:48 -0800

    Category Passes 3rd test. Good fail 4th
    Co-authored-by: johnedisc <christopher.johnedis@gmail.com>

--- ToDoList/Models/Category.cs
+++ ToDoList/Models/Category.cs
@@ -2,20 +2,25 @@ using System.Collections.Generic;
 
 namespace ToDoList.Models
 {
-  public class Category
-  {
-    private static List<Category> _instances = new List<Category> {};
-    public string Name { get; set; }
-    public int Id { get; }
-    public List<Item> Items { get; set; }
-  
-      public Category(string categoryName)
+    public class Category
     {
-      Name = categoryName;
-      _instances.Add(this);
-      Id = _instances.Count;
-      Items = new List<Item>{};
+      private static List<Category> _instances = new List<Category> { };
+      public string Name { get; set; }
+      public int Id { get; }
+      public List<Item> Items { get; set; }
+
+      public Category(string categoryName)
+      {
+          Name = categoryName;
+          _instances.Add(this);
+          Id = _instances.Count;
+          Items = new List<Item> { };
+      }
+
+      public static void ClearAll()
+      {
+          _instances.Clear();
+      }
+
     }
-    
-  }
 }
